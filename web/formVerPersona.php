b8bf4b7087cfe89fcf27c9f4bc38f0986711dfd2
tomasechague <tomas.lp@live.com>
2016-08-12 10:26:54 -0300

    cambios

---
+++ web/formVerPersona.php
@@ -0,0 +1,55 @@
+<?php
+require_once("../scripts/acceso.php");
+require_once("../functions/funciones.php");
+?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <?php
+        $rootpath = $_SERVER['DOCUMENT_ROOT'];
+
+        $path = $rootpath . '/pruebas/_partials/head.php';
+        include_once($path);
+        ?>
+    </head>
+    <body>
+
+        <?php
+        $path = $rootpath . '/pruebas/_partials/header.php';
+        include_once($path);
+
+        $path = $rootpath . '/pruebas/_partials/menu.php';
+        include_once($path);
+        $error = "";
+        $id = $_GET['id'];
+        $pdo = conectar();
+        $statement = $pdo->prepare("SELECT * FROM personas where id = $id");
+        $statement->execute();
+        $result = $statement->fetchAll();
+        
+       
+        ?>
+        <br/>
+        <br/>
+        <form class="form-inline centered" action="../web/listado.php" method="POST"> 
+
+            <input type="text" class="form-control" id="nombre"  placeholder="Nombre" name="nombre" value="<?php echo $result[0]['nombre'];?>" disabled><br/>
+
+            <input type="text" class="form-control" id="apellido" placeholder="Apellido" name="apellido" value="<?php echo $result[0]['apellido'];?>"disabled><br/>
+            
+            <input type="text" class="form-control" id="dni" placeholder="Dni" name="dni" value="<?php echo $result[0]['dni'];?>"disabled><br/>
+            <input type="hidden" class="form-control" id="id" name="id" value="<?php echo $id; ?>">
+            <input type="text" class="form-control" id="fechaNacimiento" placeholder="Fecha de Nacimiento" name="nacimiento" value="<?php echo formatearFechaNacimiento($result[0]['fecha_nacimiento']);?>"disabled><br/><br/>
+            
+            <input type="submit" class="btn btn-primary" value="Retornar al Listado">
+        </form>
+
+        <div class="centered">
+        <?php
+        $path = $rootpath . '/pruebas/_partials/footer.php';
+        include_once($path);
+        ?>
+        </div>
+    </body>
+</html>
