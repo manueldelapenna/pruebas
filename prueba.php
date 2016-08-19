305b2b57f9c6b900bc5f3aa06ea76e58e0480dfb
tomasechague <tomas.lp@live.com>
2016-08-19 14:58:12 -0300

    cambios

--- prueba.php
+++ prueba.php
@@ -1,28 +1,50 @@
-<?php 
-define('BOT_TOKEN', '267163407:AAHVbvi2N0AFTerkb3ZiDoNPf-zNI5RzDRE');
-define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
-	
-// read incoming info and grab the chatID
-$content = file_get_contents(API_URL . 'getUpdates');
-$update = json_decode($content, true);
+<?php
 
+define('BOT_TOKEN', '267163407:AAHVbvi2N0AFTerkb3ZiDoNPf-zNI5RzDRE');
+define('API_URL', 'https://api.telegram.org/bot' . BOT_TOKEN . '/');
+
+
+
+// read incoming info and grab the chatID
+$archivo = 'updateId.txt';
+if (!$compararUpdateId = file_get_contents($archivo)) {
+    die("imposible leer archivo");
+}
+$content = @file_get_contents(API_URL . 'getUpdates?offset=-1000');
+$update = json_decode($content, true);
 $results = $update['result'];
 
-foreach ($results as $result){
-	
-	// compose reply
-	$reply =  sendMessage();
+foreach ($results as $result) {
 
-	$chatID = $result["message"]["chat"]["id"];
-	
-	// send reply
-	$sendto =API_URL."sendmessage?chat_id=".$chatID."&text=".$reply;
-	file_get_contents($sendto);
+    if ($compararUpdateId < $result['update_id']) {
+        @file_put_contents($archivo, $result['update_id']);
+        // compose reply
+
+        switch ($result['message']['text']) {
+
+            case "/start":
+                $reply = sendMessage();
+                break;
+
+            case "/hombre":
+                $reply = "La persona ingresada es un hombre";
+                break;
+
+            case "/mujer" :
+                $reply = "La persona ingresada es una mujer";
+                break;
+
+            default:
+                $reply = "Los comandos permitidos son:  /start, /hombre, /mujer";
+        }
+        $chatID = $result["message"]["chat"]["id"];
+        // send reply
+        $sendto = API_URL . "sendmessage?chat_id=" . $chatID . "&text=" . $reply;
+        file_get_contents($sendto);
+    }
 }
 
-
-
-function sendMessage(){
-	$message = "Respuesta para Tomas.";
-return $message;
+function sendMessage() {
+    $message = "Respuesta para Tomas.";
+    return $message;
 }
