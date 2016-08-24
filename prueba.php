af46a4329b8f8fafdaaf236fc391b4e53eb1da16
manueldelapenna <manueldelapenna@gmail.com>
2016-08-24 10:05:41 -0300

    botones telegram

--- prueba.php
+++ prueba.php
@@ -20,34 +20,24 @@ $results = $update['result'];
 foreach ($results as $result) {
 
     // compose reply
-
-    switch ($result['message']['text']) {
-
-        case "/start":
-            $chatID = $result["message"]["chat"]["id"];
-
-            $keyboard = array(
-                'keyboard' => array(array('/hombre', '/mujer'),array('/mayor','/menor')),
-                'one_time_keyboard' => true,
-                'resize_keyboard' => true
-            );
-            $encodeado = json_encode($keyboard);
-
-            $reply = "Seleccione un boton";
-            $sendto = API_URL . "sendmessage?chat_id=" . $chatID . "&text=" . $reply . "&reply_markup=" . $encodeado;
-            file_get_contents($sendto);
-            //$reply = sendMessage();
-            break;
+	
+	if(isset($result['callback_query'])){
+		$cmd = $result['callback_query']['data'];
+		$chatID = $result['callback_query']["message"]["chat"]["id"];
+	}else{
+		$cmd = $result['message']['text'];
+		$chatID = $result["message"]["chat"]["id"];
+	}
+	
+    switch ($cmd) {
 
         case "/hombre":
-            $chatID = $result["message"]["chat"]["id"];
             $reply = "La persona ingresada es un hombre";
             $sendto = API_URL . "sendmessage?chat_id=" . $chatID . "&text=" . $reply;
             file_get_contents($sendto);
             break;
 
         case "/mujer" :
-            $chatID = $result["message"]["chat"]["id"];
             $reply = "La persona ingresada es una mujer";
             $sendto = API_URL . "sendmessage?chat_id=" . $chatID . "&text=" . $reply;
             file_get_contents($sendto);
@@ -56,7 +46,6 @@ foreach ($results as $result) {
         case "/mayor" :
             $mayorPersona = MayorDeEdad(todasPersonas());
             $reply ="  " . $mayorPersona['nombre'] . " " . $mayorPersona['apellido'] . " tiene " . $mayorPersona['anios'] . " años y es la persona mas grande de la base de datos";
-            $chatID = $result["message"]["chat"]["id"];
              $sendto = API_URL . "sendmessage?chat_id=" . $chatID . "&text=" . $reply;
             file_get_contents($sendto);
             break;
@@ -64,7 +53,6 @@ foreach ($results as $result) {
         case "/menor" :
             $menorPersona = MenorDeEdad(todasPersonas());
             $reply ="  " . $menorPersona['nombre'] . " " . $menorPersona['apellido'] . " tiene " . $menorPersona['anios'] . " años y es la persona mas chica de la base de datos";
-            $chatID = $result["message"]["chat"]["id"];
              $sendto = API_URL . "sendmessage?chat_id=" . $chatID . "&text=" . $reply;
             file_get_contents($sendto);   
             break;
@@ -75,7 +63,6 @@ foreach ($results as $result) {
                 $user['nombre']=$usuario['nombre'];
                 $user['apellido']=$usuario['apellido'];
                 $reply=  json_encode($user);
-            $chatID = $result["message"]["chat"]["id"];
              $sendto = API_URL . "sendmessage?chat_id=" . $chatID . "&text=" . $reply;
             file_get_contents($sendto);
             }
@@ -83,9 +70,38 @@ foreach ($results as $result) {
             break;
             
         default:
-            $reply = "Los comandos permitidos son:  /start, /hombre, /mujer, /mayor, /menor";
             $chatID = $result["message"]["chat"]["id"];
-             $sendto = API_URL . "sendmessage?chat_id=" . $chatID . "&text=" . $reply;
+
+            $keyboard = array(
+                'keyboard' => array(array('/hombre', '/mujer'),array('/mayor','/menor')),
+                'one_time_keyboard' => true,
+                'resize_keyboard' => true
+            );
+            $encodeado = json_encode($keyboard);
+
+			$data = array();
+			$data['text'] = "Hombre";
+			$data['callback_data'] = "/hombre";
+			
+			$data2 = array();
+			$data2['text'] = "Mujer";
+			$data2['callback_data'] = "/mujer";
+			
+			$data3 = array();
+			$data3['text'] = "Mayor";
+			$data3['callback_data'] = "/mayor";
+			
+			$data4 = array();
+			$data4['text'] = "Menor";
+			$data4['callback_data'] = "/menor";
+			
+			$keyboard = array();
+			$keyboard['inline_keyboard'] = array(array($data, $data2), array( $data3, $data4));
+			
+			$encodeado = json_encode($keyboard);
+			
+            $reply = "Seleccione un boton";
+            $sendto = API_URL . "sendmessage?chat_id=" . $chatID . "&text=" . $reply . "&reply_markup=" . $encodeado;
             file_get_contents($sendto);
             break;
             }
