5a56449ded9bf1719d5f80ef2b44611a1007d309
tomasechague <tomas.lp@live.com>
2016-09-14 09:41:09 -0300

    cambios

--- web/listado.php
+++ web/listado.php
@@ -27,15 +27,7 @@ require_once("../functions/funciones.php");
         $path = $rootpath . '/pruebas/functions/funciones.php';
         include_once($path);
 
-        $items = (isset($_GET['items'])) ? $_GET['items'] : 100;
-
-        $indicadorDireccion = (isset($_GET['direccion'])) ? direccionOrdenamiento($_GET['direccion']) : "ASC";
-        $iconoDireccion = ($indicadorDireccion == "DESC") ? "glyphicon glyphicon-circle-arrow-up" : "glyphicon glyphicon-circle-arrow-down";
-        $orden = (isset($_GET['orden'])) ? $_GET['orden'] : "id";
-        $direccion = (isset($_GET['direccion'])) ? $_GET['direccion'] : "DESC";
-        $busqueda = (isset($_GET['busqueda'])) ? $_GET['busqueda'] : "";
-        $total = totalPersonas($busqueda);
-        $cantPaginas = ceil($total / $items);
+        
         ?>
 
         <div>
@@ -56,17 +48,17 @@ require_once("../functions/funciones.php");
             <br/><br/>
             <label>Items Pagina</label>    
             <select id="cantItems">
-                <option value="5"<?php echo ($items == 5) ? "selected" : ""; ?>>5</option>
-                <option value="10"<?php echo ($items == 10) ? "selected" : ""; ?>>10</option>
-                <option value="20"<?php echo ($items == 20) ? "selected" : ""; ?>>20</option>
+                <option value="5" selected>5</option>
+                <option value="10">10</option>
+                <option value="20">20</option>
             </select>
-            <input type="hidden" value="<?php $orden ?>" id="ordenActual">
-            <input type="hidden" value="<?php echo $direccion ?>" id="direccionActual">
-            <input type="hidden" value="<?php echo $iconoDireccion ?>" id="iconoDireccion">
+            <input type="hidden" value="id" id="ordenActual">
+            <input type="hidden" value="ASC" id="direccionActual">
+            <input type="hidden" value="glyphicon glyphicon-circle-arrow-up" id="iconoDireccion">
             <table class="table">
                 <thead class="head-table">
                     <tr>
-                        <th><a name="id">ID</a></th>
+                        <th><a name="id"><span class="glyphicon glyphicon-circle-arrow-up"></span>ID</a></th>
                         <th><a name="nombre">Nombre</a></th>
                         <th><a name="apellido">Apellido</a></th>
                         <th><a name="edad">Edad</a></th>
@@ -92,7 +84,9 @@ require_once("../functions/funciones.php");
         include_once($path);
         ?>
         <script>
-            filterPerson();
+          $(document).ready(function(){
+              filterPerson();
+          });  
             var paginaActual = 1;
             
             $('#cantItems').change(function () {
