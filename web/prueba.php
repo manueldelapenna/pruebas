b1b13aea196533e9012b380dcd9c02c58b504cc9
manueldelapenna <manueldelapenna@gmail.com>
2016-09-30 09:20:51 -0300

    fix listado personas ajax

--- web/prueba.php
+++ web/prueba.php
@@ -50,10 +50,39 @@ function listar() {
 
             $("#content").html(data);
 			filterPerson();
+			
 			$('#cantItems').change(function () {
 				filterPerson();
 
 			});
+			
+			$(".pagination").on("click", "a", function () {
+				$("#paginaActual").val($(this).text());
+				filterPerson();
+			});
+			
+			$(".head-table").on("click", "a", function () {
+				//var iconoDireccion = $("#iconoDireccion").val();
+
+				//cambio de direccion
+				if ($("#direccionActual").val() === "ASC") {
+					$("#direccionActual").val("DESC");
+					$(this + ' span').removeClass('glyphicon glyphicon-circle-arrow-up');
+					$(this + '[name= ' + $(this).attr('name') + '] span').addClass('glyphicon glyphicon-circle-arrow-down');
+				} else {
+					$("#direccionActual").val("ASC");
+					$(this + ' span').removeClass('glyphicon glyphicon-circle-arrow-down');
+					$(this + '[name= ' + $(this).attr('name') + '] span').addClass('glyphicon glyphicon-circle-arrow-up');
+				}
+
+				// $(".table").find("span").remove();
+				//$(this).append("span").addClass(iconoDireccion);
+
+				//cambio el orden
+				$("#ordenActual").val($(this).attr('name'));
+				filterPerson();
+			});
+    
 
         }
 	});
